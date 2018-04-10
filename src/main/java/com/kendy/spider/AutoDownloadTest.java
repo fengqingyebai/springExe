0f63942385c524a19ebdf01ce3eca38d707a1840
greatkendy123 <1260466457@qq.com>
2018-04-10 22:50:52 +0800

    promot:优化自动下载AutoDownloadTest

--- src/main/java/com/kendy/spider/AutoDownloadTest.java
+++ src/main/java/com/kendy/spider/AutoDownloadTest.java
@@ -1,7 +1,9 @@
 package com.kendy.spider;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,25 +11,27 @@ import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 
 public class AutoDownloadTest {
+	
+	private static Logger log = Logger.getLogger(AutoDownloadTest.class);
 
     public static void main(String[] args) {
         System.out.println("开始下载...");
-        autoDown();
+        autoDown("b.xls");
         System.out.println("finishes...");
 
     }
     
     private static final String DOWN_LOAD_EXCEL_URL = "http://cms.pokermanager.club/cms-api/game/exportGame";
     
-    private static void autoDown() {
-        Map<String, String> params = getParams("28758406");
+    private static void autoDown(String fileName) {
+        Map<String, String> params = getParams("28763605");
         try {
 //            String resString = HttpUtil.sendPost(DOWN_LOAD_EXCEL_URL, params, getToken());
-            String urlString = DOWN_LOAD_EXCEL_URL + "?roomId=28759512&token=" + getToken();
-            String path = "D:/";
+            String urlString = DOWN_LOAD_EXCEL_URL + "?roomId=28763605&token=" + getToken();
+            String path = "D:/" + fileName;
             download(urlString, path);
             
         } catch (Exception e) {
@@ -36,41 +40,58 @@ public class AutoDownloadTest {
         
     }
     
-    @SuppressWarnings("deprecation")
-    public static void download(String url,String path){  
+    public static void download(String url,String path) throws Exception {  
+    	long start = System.currentTimeMillis();
         File file= null;  
-        FileOutputStream fos=null;  
-        String downloadName= "bss.xls";
         HttpURLConnection httpCon = null;  
         URLConnection  con = null;  
         URL urlObj=null;  
-        InputStream in =null;  
-        byte[] size = new byte[1024];  
-        int num=0;  
-        try {  
-            file = new File(path+downloadName);  
-            fos = new FileOutputStream(file);  
-            urlObj = new URL(url);  
-            con = urlObj.openConnection();  
-            httpCon =(HttpURLConnection) con;  
+        try {
+			file = new File(path);  
+			urlObj = new URL(url);  
+			con = urlObj.openConnection();  
+			httpCon =(HttpURLConnection) con;
+			httpCon.setConnectTimeout(15 * 1000);
+			httpCon.setReadTimeout(10 * 1000);
+		} catch (Exception e1) {
+			throw e1;
+		}  
+        
+        try(InputStream in = httpCon.getInputStream();
+        	FileOutputStream fos = new FileOutputStream(file);) {  
             int responseCode = httpCon.getResponseCode();
-            in = httpCon.getInputStream();  
-            while((num=in.read(size)) != -1){  
-                for(int i=0;i<num;i++)   
-                    fos.write(size[i]);  
-            }  
-              
+            if(responseCode != 200) {
+            	throw new Exception("自动下载返回码："+responseCode);
+            }
+            
+            //获取自己数组  
+            byte[] getData = readInputStream(in); 
+            fos.write(getData); 
+            long end = System.currentTimeMillis();
+            log.info("已成功下载,耗时：" + (end - start) + "毫秒");
+            
         } catch (Exception e) {  
-            e.printStackTrace();  
-        } finally{  
-            try {  
-                in.close();  
-                fos.close();  
-            } catch (Exception e) {  
-                e.printStackTrace();  
-            }  
-        }  
+            throw e;
+        } 
     }  
+    
+    
+    /** 
+     * 从输入流中获取字节数组 
+     * @param inputStream 
+     * @return 
+     * @throws IOException 
+     */  
+    public static  byte[] readInputStream(InputStream inputStream) throws IOException {    
+        byte[] buffer = new byte[1024 * 4];    
+        int len = 0;    
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();    
+        while((len = inputStream.read(buffer)) != -1) {    
+            bos.write(buffer, 0, len);    
+        }    
+        bos.close();    
+        return bos.toByteArray();    
+    }
     
     private static Map<String,String> getParams(String roomId) {
         Map<String,String> params = new HashMap<>();
@@ -86,11 +107,9 @@ public class AutoDownloadTest {
         
     }
     
-    
-
 
     private static String getToken() {
-        String token = "305c300d06092a864886f70d0101010500034b003048024100ab57b1ddd9a1dd6a179f6d904d0c9f9ce8e5a4f43829433f5dadcb45f39b79eb4e202b0accdfd076ac3db0ba99df0dff22e0b691943eb0a0c515b82e804b56c10203010001";
+        String token = "305c300d06092a864886f70d0101010500034b003048024100b7e2f18624017476266bdb2657ceaa66909e1f9370052c31c443d9f777e364eeb26c033ed1254a6275ccb2759b9db9c4a2350974b0f85a10426d04fb2e372cf50203010001";
         return token;
     }
 
