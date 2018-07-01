75de525b97b73cea03e735c34ea8921f8fa081c9
greatkendy123 <1260466457@qq.com>
2018-07-01 15:58:34 +0800

    全面升级POI3.14到3.17版本

--- src/main/java/com/kendy/excel/ExportShangmaExcel.java
+++ src/main/java/com/kendy/excel/ExportShangmaExcel.java
@@ -4,21 +4,16 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Date;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.CellRangeAddress;
-import org.apache.poi.hssf.util.HSSFColor;
 
 /**
  * 实时上码导出
@@ -68,8 +63,8 @@ public class ExportShangmaExcel {
 			 HSSFCell cellTiltle = rowm.createCell(0);
 			
 			//// sheet样式定义【getColumnTopStyle()/getStyle()均为自定义方法 - 在下面 - 可扩展
-			HSSFCellStyle columnTopStyle = this.getColumnTopStyle(workbook);// 获取列头样式对象
-			HSSFCellStyle style = this.getStyle(workbook); // 单元格样式对象
+			HSSFCellStyle columnTopStyle = ExcelCss.getColumnTopStyle(workbook);// 获取列头样式对象
+			HSSFCellStyle style = ExcelCss.getStyle(workbook); // 单元格样式对象
 			/************************************* 正文 ****************************/
 
 			// 定义所需列数
@@ -147,89 +142,5 @@ public class ExportShangmaExcel {
 
 	}
 
-	/*
-	 * 列头单元格样式
-	 */
-	public HSSFCellStyle getColumnTopStyle(HSSFWorkbook workbook) {
-
-		// 设置字体
-		HSSFFont font = workbook.createFont();
-		// 设置字体大小
-		font.setFontHeightInPoints((short) 11);
-		// 字体加粗
-		font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-		// 设置字体名字
-		// font.setFontName("Courier New");
-		// 设置样式;
-		HSSFCellStyle style = workbook.createCellStyle();
-		// 设置底边框;
-		style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-		// 设置底边框颜色;
-		style.setBottomBorderColor(HSSFColor.BLACK.index);
-		// 设置左边框;
-		style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-		// 设置左边框颜色;
-		style.setLeftBorderColor(HSSFColor.BLACK.index);
-		// 设置右边框;
-		style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-		// 设置右边框颜色;
-		style.setRightBorderColor(HSSFColor.BLACK.index);
-		// 设置顶边框;
-		style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-		// 设置顶边框颜色;
-		style.setTopBorderColor(HSSFColor.BLACK.index);
-		// 在样式用应用设置的字体;
-		style.setFont(font);
-		// 设置自动换行;
-		style.setWrapText(false);
-		// 设置水平对齐的样式为居中对齐;
-		style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-		// 设置垂直对齐的样式为居中对齐;
-		style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
-
-		return style;
-
-	}
-
-	/*
-	 * 列数据信息单元格样式
-	 */
-	public HSSFCellStyle getStyle(HSSFWorkbook workbook) {
-		// 设置字体
-		HSSFFont font = workbook.createFont();
-		// 设置字体大小
-		// font.setFontHeightInPoints((short)10);
-		// 字体加粗
-		// font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-		// 设置字体名字
-		// font.setFontName("Courier New");
-		// 设置样式;
-		HSSFCellStyle style = workbook.createCellStyle();
-		// 设置底边框;
-		style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-		// 设置底边框颜色;
-		style.setBottomBorderColor(HSSFColor.BLACK.index);
-		// 设置左边框;
-		style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-		// 设置左边框颜色;
-		style.setLeftBorderColor(HSSFColor.BLACK.index);
-		// 设置右边框;
-		style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-		// 设置右边框颜色;
-		style.setRightBorderColor(HSSFColor.BLACK.index);
-		// 设置顶边框;
-		style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-		// 设置顶边框颜色;
-		style.setTopBorderColor(HSSFColor.BLACK.index);
-		// 在样式用应用设置的字体;
-		style.setFont(font);
-		// 设置自动换行;
-		style.setWrapText(false);
-		// 设置水平对齐的样式为居中对齐;
-		style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-		// 设置垂直对齐的样式为居中对齐;
-		style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
-		return style;
-	}
 
 }
--- src/main/java/com/kendy/excel/ExportTGExcel.java
+++ src/main/java/com/kendy/excel/ExportTGExcel.java
@@ -76,8 +76,8 @@ public class ExportTGExcel  {
 	            HSSFRow rowColumnName = sheet.createRow(0);
 	            
 	            // sheet样式定义【getColumnTopStyle()/getStyle()均为自定义方法 - 在下面  - 可扩展
-	            HSSFCellStyle columnTopStyle = this.getColumnTopStyle(workbook);//获取列头样式对象
-	            HSSFCellStyle style = this.getStyle(workbook);                    //单元格样式对象
+	            HSSFCellStyle columnTopStyle = ExcelCss.getColumnTopStyle(workbook);//获取列头样式对象
+	            HSSFCellStyle style = ExcelCss.getStyle(workbook);                    //单元格样式对象
 	            
 	            /*************************************  标题栏  ****************************/
 	            int columnNum = columnTitleList.size();
@@ -170,8 +170,8 @@ public class ExportTGExcel  {
     	  HSSFSheet sheet = workbook.createSheet(excelModel.getSheetName());  
     	  Map<String, List<Object[]>> waizhaiMap = excelModel.getWaizhaiMap();
           
-          HSSFCellStyle columnTopStyle = this.getColumnTopStyle(workbook);//获取列头样式对象
-          HSSFCellStyle style = this.getStyle(workbook);                    //单元格样式对象
+          HSSFCellStyle columnTopStyle = ExcelCss.getColumnTopStyle(workbook);//获取列头样式对象
+          HSSFCellStyle style = ExcelCss.getStyle(workbook);                    //单元格样式对象
           
           
           int index = 1;//第几个表
@@ -223,90 +223,6 @@ public class ExportTGExcel  {
                
     }
     
-    /* 
-     * 列头单元格样式
-     */    
-      public HSSFCellStyle getColumnTopStyle(HSSFWorkbook workbook) {
-          
-            // 设置字体
-          HSSFFont font = workbook.createFont();
-          //设置字体大小
-          font.setFontHeightInPoints((short)11);
-          //字体加粗
-          font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-          //设置字体名字 
-//          font.setFontName("Courier New");
-          //设置样式; 
-          HSSFCellStyle style = workbook.createCellStyle();
-          //设置底边框; 
-          style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-          //设置底边框颜色;  
-          style.setBottomBorderColor(HSSFColor.BLACK.index);
-          //设置左边框;   
-          style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-          //设置左边框颜色; 
-          style.setLeftBorderColor(HSSFColor.BLACK.index);
-          //设置右边框; 
-          style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-          //设置右边框颜色; 
-          style.setRightBorderColor(HSSFColor.BLACK.index);
-          //设置顶边框; 
-          style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-          //设置顶边框颜色;  
-          style.setTopBorderColor(HSSFColor.BLACK.index);
-          //在样式用应用设置的字体;  
-          style.setFont(font);
-          //设置自动换行; 
-          style.setWrapText(false);
-          //设置水平对齐的样式为居中对齐;  
-          style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-          //设置垂直对齐的样式为居中对齐; 
-          style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
-          
-          return style;
-          
-      }
-      
-      /*  
-     * 列数据信息单元格样式
-     */  
-      public HSSFCellStyle getStyle(HSSFWorkbook workbook) {
-            // 设置字体
-            HSSFFont font = workbook.createFont();
-            //设置字体大小
-            //font.setFontHeightInPoints((short)10);
-            //字体加粗
-            //font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-            //设置字体名字 
-//            font.setFontName("Courier New");
-            //设置样式; 
-            HSSFCellStyle style = workbook.createCellStyle();
-            //设置底边框; 
-            style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-            //设置底边框颜色;  
-            style.setBottomBorderColor(HSSFColor.BLACK.index);
-            //设置左边框;   
-            style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-            //设置左边框颜色; 
-            style.setLeftBorderColor(HSSFColor.BLACK.index);
-            //设置右边框; 
-            style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-            //设置右边框颜色; 
-            style.setRightBorderColor(HSSFColor.BLACK.index);
-            //设置顶边框; 
-            style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-            //设置顶边框颜色;  
-            style.setTopBorderColor(HSSFColor.BLACK.index);
-            //在样式用应用设置的字体;  
-            style.setFont(font);
-            //设置自动换行; 
-            style.setWrapText(false);
-            //设置水平对齐的样式为居中对齐;  
-            style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-            //设置垂直对齐的样式为居中对齐; 
-            style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
-            return style;
-      }
 
 
       /**
