b071e333d41c2443219c6a9d89b3db6114a4029a
林泽涛 <linzt8@chinaunicom.cn>
2018-07-04 16:20:24 +0800

    add: 进度条的正确使用方式示例

---
+++ src/main/java/com/kendy/test/progress/serviceapp/GetDailySalesService.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kendy.test.progress.serviceapp;
+
+
+import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+/**
+ * 进度条的正确使用方式2
+ * service与Application thread之间通过creatTask()进行联络，而Tast肯定要返回一个东西，哪怕这个东西是Void
+ * 
+ * @author linzt
+ * @time 2018年7月4日 下午4:14:33
+ */
+public class GetDailySalesService extends Service<ObservableList<DailySales>> {
+
+    /**
+     * Create and return the task for fetching the data. Note that this method
+     * is called on the background thread (all other code in this application is
+     * on the JavaFX Application Thread!).
+     *
+     * @return A task
+     */
+    @Override
+    protected Task createTask() {
+        return new GetDailySalesTask();
+    }
+}
